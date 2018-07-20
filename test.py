734de4f069694b8e3596f859140734ff86c90da1
anton feldmann <anton.feldmann@rg.fanuc.co.jp>
2018-07-20 12:09:03 +0900

    module style

---
+++ test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import datetime
+from year import year
+
+jetzt = datetime.datetime.now()
+jahr = jetzt.year
+monat = jetzt.month
+tag = jetzt.day
+stunde = jetzt.hour
+minute = jetzt.minute
+sekunde = jetzt.second
+
+#$sternzeit = 1000 *
+#($jahr + 1 / $jahreslaenge * ($jahrestag - 1 + $stunde/24 + $min/1440) - 2063);
+
+# year 2063 is the year the humans invented the warp engine in star track 
+print(1000.0 * (jahr + 1 / year.length_of_year(jahr) *  year.minuts_in_the_year(jahr,monat,tag, stunde, minute)) - 2063)
---
+++ year/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+__author__ = "Anton Feldmann"
+__copyright__ = "Copyright 2018"
+__license__ = "MIT"
+__version__ = "0.0.1"
+__maintainer__ = "Anton Feldmann"
+__email__ = "Anton.Feldmann@gmail.com"
